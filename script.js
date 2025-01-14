99af0705a2cce30f06047603808ea349fe393e4a
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-14 11:49:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,28 +4,26 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
 }
 
 function Employee(name, age, jobTitle) {
-  Person.call(this, name, age); // call the Person constructor
-  this.jobTitle = jobTitle; // set the jobTitle property on the Employee object
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
-// Inherit from Person
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+  return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`;
 }
 
 var alice = new Person('Alice', 25);
-alice.greet(); // This should log: 'Hello, my name is Alice and I am 25 years old.'
+console.log(alice.greet());
 
 var bob = new Employee('Bob', 30, 'Manager');
-bob.jobGreet(); // This should log: 'Hello, my name is Bob, I am 30 years old, and my job title is Manager.'
+console.log(bob.jobGreet());
 
-// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
